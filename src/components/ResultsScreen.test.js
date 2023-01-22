4e2f0e56119f4820b8a6fe9a533ea7ef42c1eb4b
Fern knee <kevinjbritten@gmail.com>
2023-01-21 23:24:09 -0500

    initial commit

---
+++ src/components/ResultsScreen.test.js
@@ -0,0 +1,7 @@
+test("it should display a message when no albums are found", () => {
+  throw new Error();
+});
+
+test("it should display one card for each album that is found", () => {
+  throw new Error();
+});
