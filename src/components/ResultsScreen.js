f5a305565654b27d9baf2986cc13ef567cbc269f
Fern knee <kevinjbritten@gmail.com>
2023-01-25 09:27:40 -0500

    added tests/components

---
+++ src/components/ResultsScreen.js
@@ -0,0 +1,39 @@
+import AlbumCard from "./AlbumCard";
+
+const ResultsScreen = ({ albums }) => {
+  function joinArtists(artists) {
+    return artists.reduce((previous, current) => {
+      const separator = previous ? ", " : "";
+      return previous + separator + current.name;
+    }, "");
+  }
+
+  return (
+    <div>
+      <h1>Here are the albums we found for you:</h1>
+      {albums.map((a) => {
+        const {
+          images,
+          name,
+          total_tracks: trackCount,
+          external_urls: { spotify: externalUrl },
+          artists,
+        } = a;
+        return (
+          <AlbumCard
+            album={{
+              images,
+              name,
+              trackCount,
+              externalUrl,
+              artist: joinArtists(artists),
+            }}
+            key={name}
+          ></AlbumCard>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ResultsScreen;
