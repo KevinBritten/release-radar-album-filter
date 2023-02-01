0473be90876b1f300a1ccc8cd1d0a01392dc9111
Fern knee <kevinjbritten@gmail.com>
2023-02-01 14:14:15 -0500

    added test to mock api call

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { getTracksResponse } from "./test/testData";
 
 import App from "./App";
+import { getTracks } from "./components/ApiHelpers";
+
+jest.mock("./components/ApiHelpers.js");
 
 test("it starts displaying the setup screen", () => {
   render(<App />);
-
   const header = screen.queryByText(
     "Get only the full album releases from your Release Radar."
   );
   expect(header).toBeVisible();
 });
 
-test("it displays the results screen on playlist load", () => {
-  throw new Error();
+test("it displays the results screen on playlist load", async () => {
+  render(<App />);
+  getTracks.mockResolvedValue(getTracksResponse);
+  const continueButton = screen.getByText("Continue");
+  userEvent.click(continueButton);
+  await waitFor(() => {
+    screen.getByText("Anything In Return (Instrumentals)");
+  });
 });
