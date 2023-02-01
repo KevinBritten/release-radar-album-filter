70e351593a572a6eac7d41fc0ad4056b16a5ebf8
Fern knee <kevinjbritten@gmail.com>
2023-02-01 15:03:39 -0500

    added comments and made my playlist the default value

--- src/components/PlaylistEntry.js
+++ src/components/PlaylistEntry.js
@@ -5,19 +5,23 @@ import * as api from "./ApiHelpers.js";
 import "../styles/playlist-entry.css";
 
 const PlaylistEntry = ({ loadAlbums }) => {
-  const [playlistId, setPlaylistId] = useState("");
+  const [playlistId, setPlaylistId] = useState("37i9dQZEVXbq7HBpM8RcNy");
 
+  // return an array of albums which are not singles
   function filterAlbums(tracks) {
     return tracks
       .filter((track) => !(track.track.album["album_type"] === "single"))
       .map((track) => track.track.album);
   }
+
   async function clickFunction() {
     const token = await api.getAuthToken();
     const tracks = await api.getTracks(playlistId, token);
     const filteredAlbums = filterAlbums(tracks);
     loadAlbums(filteredAlbums);
   }
+
+  //extract the id from the string copied from Spotify
   function extractId(url) {
     let startIdx = url.indexOf("/playlist/") + 10;
     let endIdx = url.indexOf("?si=");
@@ -29,6 +33,9 @@ const PlaylistEntry = ({ loadAlbums }) => {
       <p>
         Go to your Release Radar playlist in Spotify and click “Share” &gt;
         “Copy link to playlist”. Paste the link into the box below.
+        <br />
+        <br />
+        You can also just click continue to see the albums recommended for me :)
       </p>
       <form>
         <input
@@ -38,7 +45,7 @@ const PlaylistEntry = ({ loadAlbums }) => {
           onChange={(e) => {
             setPlaylistId(extractId(e.target.value));
           }}
-          placeholder="Paste playlist link here"
+          placeholder="Paste playlist link here (ie: https://open.spotify.com/playlist/37i9dQZEVXbq7HBpM8RcNy?si=e923c767a1f342b7)"
         ></input>
         <Button onClick={clickFunction}>Continue</Button>
       </form>
