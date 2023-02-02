7944360c2339e3bbb0e4ebaa41123bfc0af0f252
Fern knee <kevinjbritten@gmail.com>
2023-02-02 11:41:43 -0500

    moved normalize before other css files, fixed layout of homepage

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "normalize.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/global.scss";
 import "./styles/mixins.scss";
-import "normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
