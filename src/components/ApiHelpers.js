65420740b4adb0c4fd256fc1053797bfaad82583
Fern knee <kevinjbritten@gmail.com>
2023-02-09 22:51:18 -0500

    add comment

--- src/components/ApiHelpers.js
+++ src/components/ApiHelpers.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+//function to call netlify function which gets data from the Spotify API. In a seperate file to create mock function in test.
 const getTracks = async (playlistId) => {
   const response = await axios.post("/.netlify/functions/getTracks/getTracks", {
     playlistId,
